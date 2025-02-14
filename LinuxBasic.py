b65866211141c5a9f5f9793b11c5f0742509a206
kamlesh sah <Sahk0292@gmail.com>
2025-02-14 22:33:32 +0545

    Add: First Commit

---
+++ LinuxBasic.py
@@ -0,0 +1,14 @@
+# print("Hello Linux");
+print("I am kamlesh And learning new things day by day");
+print("I am exploring new thing and sucks in very part of the code in VIM.");
+
+x = 2;
+y = 3;
+
+if x >2:
+    print('Hi');
+else:
+    print("NOpe");
+
+
+
